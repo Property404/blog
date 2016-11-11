d5f4db5d4cd309cbb3fd84c60baf2e9b41f5ed9f
Property404 <daganboy@gmail.com>
2016-11-11 21:48:42 +0000

    Switched to Fedora, installed mysql

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
-var express = require("express");
+const express = require("express");
+const mysql = require("mysql");
+/* Set up DB stuff */
+var connection = mysql.createConnection({
+	host: "localhost",
+	user: "root",
+	password: "hunter2"
+});
+connection.connect();
+connection.query("CREATE DATABASE IF NOT EXISTS Blog;");
+connection.connect(
+
+/* Set up app */
 var app = express();
 //Set embedded JS view engine 
 app.set("view engine", "ejs");
