6d4851f853b237f14056f9d8662951010d5c6dc6
Property404 <daganboy@gmail.com>
2016-11-14 20:23:02 +0000

    Main page shows up stuff now

--- app.js
+++ app.js
@@ -4,15 +4,21 @@ const mysql = require("mysql");
 var connection = mysql.createConnection({
 	host: "localhost",
 	user: "root",
-	password: "hunter2"
+	password: "hunter2",
+	database: "Blog"
 });
 connection.connect();
-connection.query("CREATE DATABASE IF NOT EXISTS Blog;");
-connection.connect(
+connection.query("CREATE TABLE IF NOT EXISTS Article (\
+id int NOT NULL AUTO_INCREMENT,\
+timestamp timestamp NOT NULL,\
+title varchar(128),\
+body varchar(4096),\
+PRIMARY KEY(id));");
 
+//bah abah bah
 /* Set up app */
 var app = express();
-//Set embedded JS view engine 
+//~Set embedded JS view engine 
 app.set("view engine", "ejs");
 app.set("views", "./views");
 // Static Pages
@@ -20,7 +26,13 @@ app.use("/static", express.static("./public"));
 
 // Dynamic Pages 
 app.get("/", function(req, res) {
-	res.render("index", {});
+	/* Get articles */
+	connection.query("SELECT title, body FROM Article;", function(err, results){
+		console.log(err);
+		console.log(results);
+		/* Render */
+		res.render("index", {"articles": results});
+	});
 });
 
 // Serve 
